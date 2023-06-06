a37dd80b49adf5f9f6f4f68a674e2cafa44d5e5c
alexgoit <alexandrpuryaev@gmail.com>
2023-06-06 14:21:29 +0300

    Add forecast for 3 days

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import dateFormat, { masks } from 'dateformat';
-// import forecast from '../json/forecast.json';
+import forecast from '../json/forecast.json';
 
 export class Weather {
   constructor() {
@@ -13,12 +13,13 @@ export class Weather {
     });
 
     this.rootEl = document.getElementById('root');
-    this.containerEl = document.createElement('div');
-    this.containerEl.classList.add('container');
+    // this.containerEl = document.createElement('div');
+    // this.containerEl.classList.add('container');
 
-    this.rootEl.append(this.containerEl);
+    // this.rootEl.append(this.containerEl);
 
     this.createCurrentWeather();
+    this.createForecastDay();
     this.getForecast();
     this.startTimer();
   }
@@ -72,7 +73,49 @@ export class Weather {
       this.currentDescriptWrapperEl
     );
 
-    this.containerEl.append(this.currentWrapperEl);
+    const currentContainerEl = document.createElement('div');
+    currentContainerEl.classList.add('container');
+    currentContainerEl.append(this.currentWrapperEl);
+
+    this.rootEl.insertAdjacentElement('beforeend', currentContainerEl);
+  }
+
+  createForecastDay() {
+    // this.forecasteDayDateEl = document.createElement('p');
+    // this.forecasteDayDateEl.classList.add('forecast-day-date');
+
+    // this.forecasteDayTempMinEl = document.createElement('p');
+    // this.forecasteDayTempMinEl.classList.add('forecast-day-temp-min');
+
+    // this.forecasteDayTempMaxEl = document.createElement('p');
+    // this.forecasteDayTempMaxEl.classList.add('forecast-day-temp-max');
+
+    // this.forecastDayIconEl = document.createElement('img');
+    // this.forecastDayIconEl.classList.add('forecast-day-icon');
+
+    // const forecastDayTempWrapperEl = document.createElement('div');
+    // forecastDayTempWrapperEl.classList.add('forecast-day-temp-wrapper');
+    // forecastDayTempWrapperEl.append(
+    //   this.forecasteDayTempMaxEl,
+    //   this.forecasteDayTempMinEl
+    // );
+
+    // const forecastDayWrapperEl = document.createElement('div');
+    // forecastDayWrapperEl.classList.add('forecast-wrapper');
+    // forecastDayWrapperEl.append(
+    //   this.forecasteDayDateEl,
+    //   forecastDayTempWrapperEl,
+    //   this.forecastDayIconEl
+    // );
+
+    this.forecastDayListEl = document.createElement('ul');
+    this.forecastDayListEl.classList.add('forecast-day__list');
+
+    const forecastDayContainerEl = document.createElement('div');
+    forecastDayContainerEl.classList.add('container');
+    forecastDayContainerEl.append(this.forecastDayListEl);
+
+    this.rootEl.insertAdjacentElement('beforeend', forecastDayContainerEl);
   }
 
   updateCurrentWeatherUI({ current, location }) {
@@ -84,7 +127,7 @@ export class Weather {
     const { name, region, country } = location;
 
     // Current Weather Data ===================================================
-    this.currentTimeEl.textContent = this.dateConverter(last_updated_epoch);
+    this.currentTimeEl.textContent = this.datetimeConverter(last_updated_epoch);
     this.currentTempEl.textContent = `${temp_c}°C`;
     this.currentIconEl.src = `http:${icon}`;
     this.currentIconEl.alt = text;
@@ -94,6 +137,41 @@ export class Weather {
     this.locationNameEl.textContent = name;
     this.locationRegionEl.textContent = region;
     this.locationCountryEl.textContent = country;
+  }
+
+  updateForecastDayUI({ forecast: { forecastday } }) {
+    // this.forecasteDayDateEl.textContent = this.dateConverter(
+    //   forecastday[0].date_epoch
+    // );
+    // this.forecasteDayTempMinEl.textContent = `${forecastday[0].day.mintemp_c}°C`;
+    // this.forecasteDayTempMaxEl.textContent = `${forecastday[0].day.maxtemp_c}°C`;
+    // this.forecastDayIconEl.src = `https://${forecastday[0].day.condition.icon}`;
+    // this.forecastDayIconEl.alt = forecastday[0].day.condition.text;
+
+    console.dir(forecastday);
+
+    const forecastMurkup = forecastday
+      .map(forecast => {
+        return `<li class="forecast-day__item">
+        <p class="forecast-day__date">${this.dateConverter(
+          forecast.date_epoch
+        )}</p>
+        <div class="forecast-day__temp-wrapper">
+          <p class="forecast-day__temp-max">Max: ${forecast.day.maxtemp_c}°C</p>
+          <p class="forecast-day__temp-min">Min: ${forecast.day.mintemp_c}°C</p>
+        </div>
+        <img
+          src="https://${forecast.day.condition.icon}"
+          alt="${forecast.day.condition.text}"
+          class="forecast-day__icon"
+        />
+      </li>`;
+      })
+      .join('');
+
+    console.log(forecastMurkup);
+
+    this.forecastDayListEl.innerHTML = forecastMurkup;
   }
 
   startTimer() {
@@ -115,13 +193,19 @@ export class Weather {
 
   async getForecast() {
     const forecast = await this.fetchCurrentWeather('Kryvyy Rih');
+
     this.updateCurrentWeatherUI(forecast.data);
+    this.updateForecastDayUI(forecast.data);
+
+    // this.updateCurrentWeatherUI(forecast);
+    // this.updateForecastDayUI(forecast);
   }
 
   async fetchCurrentWeather(city) {
     const params = {
       key: '8ddc4d4caf994c91928163714230406',
       q: city,
+      days: '3',
       aqi: 'no',
       lang: 'uk',
     };
@@ -133,9 +217,15 @@ export class Weather {
     }
   }
 
-  dateConverter(timestamp) {
+  datetimeConverter(timestamp) {
     const now = new Date(timestamp * 1000);
 
     return dateFormat(now, 'dd.mm.yyyy HH:MM');
+  }
+
+  dateConverter(timestamp) {
+    const now = new Date(timestamp * 1000);
+
+    return dateFormat(now, 'dd.mm.yyyy');
   }
 }
